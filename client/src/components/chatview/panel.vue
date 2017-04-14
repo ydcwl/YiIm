746b37a13ec607fba78eaff20a2cd9e653bf9b98
ydcwl <13420311953@163.com>
2017-04-14 17:12:30 +0800

    前端布局基本完成

--- client/src/components/chatview/panel.vue
+++ client/src/components/chatview/panel.vue
@@ -1,61 +1,91 @@
 <template>
   <section class="all">
-    <div class="user-panel" @mousedown="down" @mousemove="move" @mouseup="up">
-      <div class="user-panel-header">
+    <!--用户面板-->
+    <div class="user-panel">
+      <div class="user-panel-header" @mousedown="down" @mousemove="move" @mouseup="up">
         <div class="user-panel-img">
           <img src="../../assets/uploadImg.png">
         </div>
         <p class="user-panel-name">李小明</p>
+        <div class="window-tool-con">
+          <img src="../../assets/reduce.png" alt="" class="window-tool">
+          <img src="../../assets/close.png" alt="" class="window-tool">
+        </div>
+      </div>
+      <div class="user-panel-list-con">
+        <p class="user-panel-list" @dblclick="chat">
+          <img src="../../assets/logo.png"> 我走了
+        </p>
+        <p class="user-panel-list">
+          <img src="../../assets/uploadImg.png"> 我走了
+        </p>
       </div>
     </div>
+    <!--用户面板结束-->
+    <!--聊天界面-->
+    <talk-view></talk-view>
+    <!--聊天界面结束-->
   </section>
 </template>
 <script>
-  export default{
+  import TalkView from './talkView.vue'
+  export default {
+    components: {
+      'talk-view': TalkView
+    },
     data() {
       return {
         flag: false,
-        offset: 0
+        offsetX: 0,
+        offsetY: 0
       }
     },
     methods: {
       down() {
         this.flag = true;
-        let self = event.currentTarget;
-        let offsetx = self.style.left;
-        let x = event.clientX;
-        this.offset = x - offsetx;
-
+        this.offsetX = event.offsetX;
+        this.offsetY = event.offsetY;
       },
       move() {
-       if(this.flag) {
-         let self = event.currentTarget;
-        let x = event.clientX - this.offset;
-        self.style.left = x + 'px';
-        console.log(event)
-       }
+        if (this.flag) {
+          let self = event.currentTarget.parentNode;
+          let x = event.clientX - this.offsetX;
+          let y = event.clientY - this.offsetY;
+          self.style.left = x + 'px';
+          self.style.top = y + 'px';
+        }
       },
       up() {
         this.flag = false;
+      },
+      chat() {
+        console.log('dbclick')
       }
     }
   }
+
 </script>
 <style lang="scss" scoped>
-  .all{
+  .all {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
   }
   /*用户面板*/
-  .user-panel{
+
+  .user-panel {
     position: absolute;
     width: 250px;
     height: 600px;
-    border: 1px solid 4C6983;
     right: 50px;
-    top: 50%;
-    margin-top: -300px;
+    top: 20px;
     background: #f0f0f0;
+    z-index: 999;
+    box-shadow: 0px 0px 5px 1px #4C6976
   }
-  .user-panel-header{
+
+  .user-panel-header {
     width: 100%;
     height: 150px;
     display: flex;
@@ -63,18 +93,81 @@
     flex-wrap: wrap;
     align-items: center;
     border-bottom: 1px solid #ccc;
+    cursor: move;
+    background: #ccc;
   }
-  .user-panel-img{
+  /*窗口工具*/
+
+  .user-panel-header .window-tool-con {
+    position: absolute;
+    right: 0px;
+    top: 10px;
+    height: 14px;
+    display: flex;
+    cursor: pointer;
+  }
+
+  .user-panel-header .window-tool-con img {
+    width: 14px;
+    margin-right: 8px;
+  }
+  .user-panel-header .window-tool-con img:hover{
+    background: #aaa;
+  }
+
+  .user-panel-img {
     width: 80px;
     height: 80px;
     border-radius: 50%;
     overflow: hidden;
   }
+
   .user-panel-img img {
     width: 100%;
+    cursor: pointer;
   }
-  .user-panel-name{
+
+  .user-panel-name {
     width: 100%;
     text-align: center;
   }
+  /*用户列表*/
+
+  .user-panel-list-con {
+    width: 100%;
+    position: absolute;
+    top: 150px;
+    bottom: 0px;
+    overflow: auto;
+  }
+
+  .user-panel-list {
+    width: 100%;
+    border-bottom: 1px solid #ccc;
+    padding: 10px 0px;
+    cursor: pointer;
+  }
+  /*.user-panel-list:last-child{
+     border: 0px;
+   }*/
+
+  .user-panel-list img {
+    width: 40px;
+    vertical-align: middle;
+    margin: 0px 10px;
+  }
+  /*滚动条样式*/
+
+   ::-webkit-scrollbar {
+    width: 5px;
+    background: #f0f0f0;
+    border-radius: 2px;
+  }
+
+   ::-webkit-scrollbar-thumb {
+    width: 5px;
+    background: #aaa;
+    border-radius: 2px;
+  }
+
 </style>
---
+++ client/src/components/chatview/talkView.vue
@@ -0,0 +1,193 @@
+<template>
+  <section class="talk-view">
+    <!--左边区域-->
+    <div class="talk-view-left">
+      <p class="talk-user-list">
+        <img src="../../assets/logo.png"> 我走了<img src="../../assets/close.png" class="close-view">
+      </p>
+      <p class="talk-user-list">
+        <img src="../../assets/uploadImg.png"> 我走了<img src="../../assets/close.png" class="close-view">
+      </p>
+    </div>
+    <!--右边区域-->
+    <div class="talk-view-right">
+      <div class="header" @mousedown="down" @mousemove="move" @mouseup="up">
+        <img src="../../assets/uploadImg.png">我走了
+        <div class="window-tool-con">
+          <img src="../../assets/reduce.png" alt="" class="window-tool">
+          <img src="../../assets/close.png" alt="" class="window-tool">
+        </div>
+      </div>
+      <div class="show-view">
+        <p class="talk-list"></p>
+      </div>
+      <div class="edit">
+        <div class="talk-tool-con">
+          <img src="../../assets/face.png">
+          <img src="../../assets/img.png">
+        </div>
+        <div class="edit-text" contenteditable="true">
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        flag: false
+      }
+    },
+    methods: {
+      down() {
+        this.flag = true;
+        this.offsetX = event.offsetX;
+        this.offsetY = event.offsetY;
+      },
+      move() {
+        if (this.flag) {
+          let self = event.currentTarget.parentNode.parentNode;
+          let x = event.clientX - this.offsetX - 210;
+          let y = event.clientY - this.offsetY;
+          self.style.left = x + 'px';
+          self.style.top = y + 'px';
+        }
+      },
+      up() {
+        this.flag = false;
+      },
+    }
+  }
+
+</script>
+<style lang="scss" scoped>
+  .talk-view {
+    width: 700px;
+    height: 500px;
+    position: absolute;
+    left: 100px;
+    top: 50px;
+    display: flex;
+    justify-content: center;
+  }
+  /*左区域*/
+
+  .talk-view-left {
+    width: 30%;
+    height: 100%;
+    background: #ccc;
+    overflow: auto;
+  }
+
+  .talk-view-left .talk-user-list {
+    width: 90%;
+    margin-left: 5%;
+    height: 40px;
+    padding: 5px 0px;
+    margin-top: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    position: relative;
+  }
+
+  .talk-view-left .talk-user-list:hover {
+    background: #bbb;
+  }
+
+  .talk-view-left .talk-user-list img {
+    width: 40px;
+    margin: 0px 5px;
+    vertical-align: middle;
+  }
+   .talk-view-left .talk-user-list .close-view{
+     width: 14px;
+     position: absolute;
+     right: 5px;
+     top: 16px;
+     display: none;
+   }
+   .talk-view-left .talk-user-list:hover .close-view{
+     display: block;
+   }
+
+  /*右区域*/
+
+  .talk-view-right {
+    width: 70%;
+    height: 100%;
+    background: #f0f0f0;
+  }
+
+  .talk-view-right .header {
+    width: 100%;
+    height: 70px;
+    background: #ddd;
+    line-height: 70px;
+    cursor: move;
+  }
+
+  .talk-view-right .header img {
+    width: 40px;
+    margin: 0px 10px;
+    vertical-align: middle;
+  }
+  /*窗口工具*/
+.talk-view-right .header .window-tool-con{
+  position: absolute;
+  right: 0px;
+  top: 10px;
+  height: 14px;
+  display: flex;
+  cursor: pointer;
+}
+.talk-view-right .header .window-tool-con img {
+  width: 14px;
+}
+ .talk-view-right .header .window-tool-con img:hover{
+    background: #aaa;
+  }
+
+  .talk-view-right .show-view {
+    width: 100%;
+    height: 300px;
+    overflow: auto;
+  }
+
+  .talk-view-right .edit {
+    width: 100%;
+    height: 130px;
+    border-top: 1px solid #ccc;
+  }
+
+  .talk-view-right .edit .talk-tool-con {
+    width: 100%;
+    height: 20px;
+    padding: 5px 0px;
+  }
+
+  .talk-view-right .edit .talk-tool-con img {
+    height: 100%;
+    margin-left: 10px;
+  }
+
+  .edit-text {
+    width: 100%;
+    height: 100px;
+    outline: none;
+    padding: 5px;
+    font-size: 14px;
+  }
+
+   ::-webkit-scrollbar {
+    width: 4px;
+  }
+
+   ::-webkit-scrollbar-thumb {
+    width: 4px;
+    background: #aaa;
+    border-radius: 2px;
+  }
+
+</style>
